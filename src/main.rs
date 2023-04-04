8f9aa46b8935ccaac707f1c85c2995372795b5d3
Khanh Ha <khanhhaquang@gmail.com>
2023-04-04 13:44:35 +0700

    update: handle 4040

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use reqwest::Error;
 use tokio::runtime::Runtime;
 use serde::{Deserialize,Serialize};
 
@@ -12,21 +11,42 @@ struct Cli {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-
 struct CoinInfo {
     prices: [[f64; 2]; 1],
     market_caps: [[f64; 2]; 1],
     total_volumes: [[f64; 2]; 1]
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct CoinInfoError {
+    error: String
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+enum ResponseType {
+  Ok(CoinInfo),
+  Err(CoinInfoError),
+}
+
 const CURRENCY: &str = "usd";
 
-async fn check_from_coingecko(name: &String) -> Result<CoinInfo, Error> {
+async fn check_from_coingecko(name: &String) {
     let api_url = format!("https://api.coingecko.com/api/v3/coins/{}/market_chart?vs_currency={}&days=0", name.to_lowercase(), CURRENCY);
-    let resp = reqwest::get(api_url).await?.json::<CoinInfo>().await?;
-    println!("-- Price -- : {:.2} {}", resp.prices[0][1], CURRENCY.to_uppercase());
-    println!("-- Market Cap -- : {:.2} {}", resp.market_caps[0][1], CURRENCY.to_uppercase());
-    Ok(resp)
+    let resp = reqwest::get(api_url).await.unwrap();
+
+    match resp.status() {
+        reqwest::StatusCode::OK => {
+            match resp.json::<CoinInfo>().await {
+                Ok(data) => {
+                    println!("-- Price -- : {:.2} {}", data.prices[0][1], CURRENCY.to_uppercase());
+                    println!("-- Market Cap -- : {:.2} {}", data.market_caps[0][1], CURRENCY.to_uppercase());
+                },
+                Err(_) => println!("Parsing error")
+            }
+        }
+        _ => println!("Coin not found")
+    }
 }
 
 fn main() {
@@ -34,5 +54,5 @@ fn main() {
     println!("Checking {} ...", args.name.to_uppercase());
 
     let rt = Runtime::new().unwrap();
-    rt.block_on(check_from_coingecko(&args.name)).unwrap();
+    rt.block_on(check_from_coingecko(&args.name));
 }
